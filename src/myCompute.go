55ffb2e2d55c7837426a0a7dbb509f71f63acebd
dkzhang@Office.PC <527656264@qq.com>
2019-09-18 08:58:32 +0800

    Init

---
+++ src/myCompute.go
@@ -0,0 +1,9 @@
+package main
+
+func MyAdd(a, b int) int {
+	return a + b
+}
+
+func MyMutiply(a, b int) int {
+	return a + b + 5
+}
---
+++ src/myCompute_test.go
@@ -0,0 +1,21 @@
+package main
+
+import "testing"
+
+func TestMyAdd(t *testing.T) {
+	a := 1
+	b := 2
+	c := 3
+	if MyAdd(a, b) != c {
+		t.Errorf("%d + %d =, excepted %d, got %d", a, b, c, MyAdd(a, b))
+	}
+}
+
+func TestMyMutiply(t *testing.T) {
+	a := 2
+	b := 3
+	c := 6
+	if MyMutiply(a, b) != c {
+		t.Errorf("%d + %d =, excepted %d, got %d", a, b, c, MyMutiply(a, b))
+	}
+}
